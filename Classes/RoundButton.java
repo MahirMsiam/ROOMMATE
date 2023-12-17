12519b7c234343c67efcef780d70c422ea3f82b7
Mahima-Roz <mahiafroz075@gmail.com>
2023-12-18 02:35:36 +0600

    Create RoundButton.java

---
+++ Classes/RoundButton.java
@@ -0,0 +1,34 @@
+package Classes;
+
+import javax.swing.*;
+import java.awt.*;
+
+class RoundButton extends JButton {
+    public RoundButton(String text) {
+        super(text); 
+        setContentAreaFilled(false); 
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (getModel().isArmed()) { // Checks if the button is currently pressed
+            g.setColor(Color.lightGray); // If pressed, sets  light gray
+        } else {
+            g.setColor(getBackground()); // If not pressed, sets button's background color
+        }
+        // Draws a filled round rectangle representing the button 
+        g.fillRoundRect(0, 0, getWidth() - 5, getHeight() - 5, 20, 20);
+        super.paintComponent(g); 
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        g.setColor(getForeground()); // Sets the color of the border to the button's foreground color
+        g.drawRoundRect(0, 0, getWidth() - 5, getHeight() - 5, 20, 20);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(100, 40); // Sets the preferred size of the button
+    }
+}
