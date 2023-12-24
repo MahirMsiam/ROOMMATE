9a7f47349c04fe3ade20e61bcb795bdd2e0dfa00
DrDoom77 <23-50881-1@student.aiub.edu>
2023-12-25 00:23:53 +0600

    A

--- Landlord/LandLordLogin.java
+++ Landlord/LandLordLogin.java
@@ -17,9 +17,9 @@ public class LandLordLogin implements ActionListener {
     JFrame loginFrame;
     JPanel loginDashboard;
     JLabel background;
-    JLabel userlabel, passlabel;
+    JLabel emaillabel, passlabel;
     JPasswordField PasswordField;
-    JTextField UserTextfield, UserPasswordField;
+    JTextField EmailTextfield, UserPasswordField;
     JButton login;
     JButton signup;
     JButton ForgotPass;
@@ -41,24 +41,24 @@ public class LandLordLogin implements ActionListener {
 
         // login dashboard panel
         loginDashboard = new JPanel();
-        userlabel = new JLabel("Username:");
-        Font smallFont = userlabel.getFont().deriveFont(Font.PLAIN, 18f);
-        userlabel.setBounds(610, 245, 150, 20);
-        userlabel.setForeground(Color.BLACK);
-        userlabel.setFont(smallFont);
+        emaillabel = new JLabel("Email:");
+        Font smallFont = emaillabel.getFont().deriveFont(Font.PLAIN, 18f);
+        emaillabel.setBounds(610, 245, 150, 20);
+        emaillabel.setForeground(Color.BLACK);
+        emaillabel.setFont(smallFont);
 
         passlabel = new JLabel("Password:");
         passlabel.setBounds(610, 315, 150, 20);
         passlabel.setForeground(Color.BLACK);
         passlabel.setFont(smallFont);
 
-        UserTextfield = new JTextField("", 2);
-        UserTextfield.setBounds(725, 242, 300, 32);
+        EmailTextfield = new JTextField("", 2);
+        EmailTextfield.setBounds(725, 242, 300, 32);
 
-        UserTextfield.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
-        Font bigFont = UserTextfield.getFont().deriveFont(Font.PLAIN, 20);
-        UserTextfield.setFont(bigFont);
-        UserTextfield.setOpaque(false);
+        EmailTextfield.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        Font bigFont = EmailTextfield.getFont().deriveFont(Font.PLAIN, 20);
+        EmailTextfield.setFont(bigFont);
+        EmailTextfield.setOpaque(false);
         // usertf.setContentAreaFilled(false);
         // usertf.setBorderPainted(false);
 
@@ -122,9 +122,9 @@ public class LandLordLogin implements ActionListener {
         loginDashboard.add(exit);
         loginDashboard.add(showPassword);
 
-        loginDashboard.add(userlabel);
+        loginDashboard.add(emaillabel);
         loginDashboard.add(passlabel);
-        loginDashboard.add(UserTextfield);
+        loginDashboard.add(EmailTextfield);
         loginDashboard.add(PasswordField);
         loginDashboard.add(background);
 
@@ -159,13 +159,15 @@ public class LandLordLogin implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == login) {
-            String user = UserTextfield.getText();
+            String email = EmailTextfield.getText();
             String pass = new String(PasswordField.getPassword());
             // validating data from txt file by checking hashmap
-            if (user.isEmpty() || pass.isEmpty()) {
+            if (email.isEmpty() || pass.isEmpty()) {
                 showMessageDialog(null, "Both fields are required!");
                 // add another condition to check for empty field
-            } else if (validateLogin(user, pass)) {
+            } else if (!isValidEmail(email)) {
+                showMessageDialog(null, "Invalid email!");
+            } else if (validateLogin(email, pass)) {
                 showMessageDialog(null, "Login Successful");
                 LandLordDashboard frame = new LandLordDashboard();
                 frame.setVisible(true);
@@ -198,7 +200,7 @@ public class LandLordLogin implements ActionListener {
 
 
     // userpass validation methode
-    private boolean validateLogin(String user, String pass) {
+    private boolean validateLogin(String email, String pass) {
         try (Scanner scanner = new Scanner(new File("Data\\LandLordData.txt"))) {
             StringBuilder userData = new StringBuilder();
 
@@ -208,13 +210,19 @@ public class LandLordLogin implements ActionListener {
             }
             // Check if the entered credentials exist in the data
             String userCredentials = userData.toString();
-            return userCredentials.contains("Name: " + user) &&
+            return userCredentials.contains("Name: " + email) &&
                     userCredentials.contains("Password: " + pass);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
         return false; // Unable to validate login
+    }
+
+    public boolean isValidEmail(String email) {
+
+        String emaicheck = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        return email.matches(emaicheck);
     }
 
 
