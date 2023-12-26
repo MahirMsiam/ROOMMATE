7966b67d6e4316be9cd39ab44352df7ce6949de1
Mahir Mahmud Siam <mahirmahmudsiam@gmail.com>
2023-12-27 04:30:14 +0600

    Update LandLordDashboard.java

--- Landlord/LandLordDashboard.java
+++ Landlord/LandLordDashboard.java
@@ -71,3 +71,8 @@ public class LandLordDashboard extends JFrame implements ActionListener {
         }
     }
 }
+/*HAPPY CODING
+If you liked it and it helped you drp a star
+Every Method is open for all just copy and paste
+DM freely on my socials in case you need help
+*/
