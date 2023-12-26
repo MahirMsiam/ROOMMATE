25d81db56af0b535ef4cbef5629a0834450e9b85
Mahir Mahmud Siam <mahirmahmudsiam@gmail.com>
2023-12-27 04:29:44 +0600

    Update TenantDashboard.java

--- Classes/TenantDashboard.java
+++ Classes/TenantDashboard.java
@@ -11,16 +11,15 @@ public class TenantDashboard extends JFrame {
     private JTable propertyTable;
 
     public TenantDashboard() {
-        setTitle("Property Dashboard");
-        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Tenant-Dashboard");
 
-        // Create table model with column names
+        // creating table
         DefaultTableModel tableModel = new DefaultTableModel();
         tableModel.addColumn("Name");
         tableModel.addColumn("Location");
         tableModel.addColumn("Rent");
 
-        // Read property details from the text file
+        // reading details from txt
         File file = new File("Apartments\\Property.txt");
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
@@ -44,34 +43,27 @@ public class TenantDashboard extends JFrame {
             e.printStackTrace();
         }
 
-        // Create a label for the title "Properties"
+        // label for the title "Properties"
         JLabel titleLabel = new JLabel("Properties");
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
         add(titleLabel, BorderLayout.NORTH);
-        // ...
 
-        // Set the icon image
+        // Seting the icon image
         ImageIcon icon = new ImageIcon("Media\\Frmlogo.png");
         setIconImage(icon.getImage());
 
-        setSize(1100,800);
-        // ...
-        // Create the JTable with the model
+        setSize(1100, 800);
+
+        // JTable with tablemodel
         propertyTable = new JTable(tableModel);
 
-        // Add the JTable to a scroll pane
-        /*
-         * JScrollPane scrollPane = new JScrollPane(propertyTable);
-         * add(scrollPane);
-         */
-
-        // Add the JTable to a scroll pane and set the size of the table
+        // adds scroll pane to JLable
         JScrollPane scrollPane = new JScrollPane(propertyTable);
         propertyTable.setPreferredScrollableViewportSize(new Dimension(800, 400)); // Set your desired table size here
         add(scrollPane, BorderLayout.CENTER);
 
-        // Create a "Show Details" button
+        // Show button with action
         JButton showDetailsButton = new JButton("Show Details");
         showDetailsButton.setBackground(new Color(135, 206, 235));
 
@@ -93,7 +85,7 @@ public class TenantDashboard extends JFrame {
             }
         });
 
-        // Create a "Confirm Booking" button
+        // confirm button part1 adds deails to temp file
         JButton confirmBookingButton = new JButton("Confirm Booking");
         confirmBookingButton.setBackground(new Color(135, 206, 235));
         confirmBookingButton.addActionListener(new ActionListener() {
@@ -117,10 +109,11 @@ public class TenantDashboard extends JFrame {
                     ex.printStackTrace();
                 }
 
-                // Rename the temporary file to the original file
+                // renaming temp file to the origin file
                 tempFile.renameTo(existingFile);
             }
 
+            // part2 removes from details+img
             @Override
             public void actionPerformed(ActionEvent e) {
                 int selectedRow = propertyTable.getSelectedRow();
@@ -129,19 +122,14 @@ public class TenantDashboard extends JFrame {
                     String location = (String) propertyTable.getValueAt(selectedRow, 1);
                     String rent = (String) propertyTable.getValueAt(selectedRow, 2);
 
-                    // Remove property details from the existing file
                     removePropertyFromExistingFile(name, location, rent);
-
-                    // Remove image file
                     removeImageFile(name);
 
-                    // Display booking confirmation message
+                    // display msg
                     JOptionPane.showMessageDialog(TenantDashboard.this,
                             "Booking confirmed for property: " + name,
                             "Booking Confirmation", JOptionPane.INFORMATION_MESSAGE);
-
-                    // Update the table model to reflect changes
-                    tableModel.removeRow(selectedRow);
+                    tableModel.removeRow(selectedRow);// instant removal
                 } else {
                     JOptionPane.showMessageDialog(TenantDashboard.this,
                             "Please select a property to confirm booking.",
@@ -150,42 +138,31 @@ public class TenantDashboard extends JFrame {
             }
         });
 
-        // Add the buttons to the frame
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(showDetailsButton);
         buttonPanel.add(confirmBookingButton);
         add(buttonPanel, BorderLayout.SOUTH);
 
-        // Set up the JFrame
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
+    //////// *IMPORTANT METHODS *////////////
+
     private void showPropertyDetails(String name, String location, String rent) {
         JFrame detailsFrame = new JFrame("Property Details");
         detailsFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        // Set the content pane to a panel with a background image
-        JPanel contentPane = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                ImageIcon background = new ImageIcon("Apartments\\"); // Replace "background.jpg" with your image
-                g.drawImage(background.getImage(), 0, 0, getWidth(), getHeight(), this);
-            }
-        };
-
-        // Create panel for property information with GridBagLayout
+        // property information with GridBagLayout
         JPanel detailsPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.anchor = GridBagConstraints.WEST;
         gbc.insets = new Insets(5, 5, 5, 5);
 
-        // Use Roboto Bold font
         Font robotoBoldFont = new Font("Roboto", Font.BOLD, 16);
 
-        // Add labels to detailsPanel with Roboto Bold font
+        // labels to detailsPanel
         addDetailLabel(detailsPanel, gbc, "Name:", robotoBoldFont, 0, 0);
         addDetailLabel(detailsPanel, gbc, name, robotoBoldFont, 1, 0);
         addDetailLabel(detailsPanel, gbc, "Location:", robotoBoldFont, 0, 1);
@@ -193,17 +170,16 @@ public class TenantDashboard extends JFrame {
         addDetailLabel(detailsPanel, gbc, "Rent:", robotoBoldFont, 0, 2);
         addDetailLabel(detailsPanel, gbc, rent, robotoBoldFont, 1, 2);
 
-        // Create image label
+        // scaled image label
         ImageIcon imageIcon = new ImageIcon(new ImageIcon("Apartments/" + name + ".jpg")
                 .getImage().getScaledInstance(500, 300, Image.SCALE_DEFAULT));
         JLabel imageLabel = new JLabel(imageIcon);
 
-        // Create a panel for image and property information
+        // panel for image and property info
         JPanel imageAndDetailsPanel = new JPanel(new BorderLayout());
         imageAndDetailsPanel.add(imageLabel, BorderLayout.WEST);
         imageAndDetailsPanel.add(detailsPanel, BorderLayout.CENTER);
 
-        // Create a "Back" button
         JButton backButton = new JButton("Back");
         backButton.setBackground(new Color(255, 102, 102));
         backButton.addActionListener(new ActionListener() {
@@ -213,25 +189,24 @@ public class TenantDashboard extends JFrame {
             }
         });
 
-        // Add components to the detailsFrame
-        detailsFrame.setContentPane(contentPane);
+        // components to detailsFrame
+        // detailsFrame.setContentPane(contentPane);
         detailsFrame.setLayout(new BorderLayout());
         detailsFrame.add(imageAndDetailsPanel, BorderLayout.CENTER);
         detailsFrame.add(backButton, BorderLayout.SOUTH);
 
-        // Set the size of the detailsFrame
         detailsFrame.setSize(800, 400);
 
-        // Set up the detailsFrame
         detailsFrame.setLocationRelativeTo(null);
         detailsFrame.setVisible(true);
     }
 
-    // Helper method to add detail labels to the detailsPanel with a specified font
+    // adds detail labels to the detailsPanel
+    // NB. this is to make code orgaized and easy to understand
     private void addDetailLabel(JPanel detailsPanel, GridBagConstraints gbc, String text, Font font, int gridx,
             int gridy) {
         JLabel label = new JLabel(text);
-        label.setFont(font); // Set font
+        label.setFont(font);
         detailsPanel.add(label, gbc);
         gbc.gridx = gridx;
         gbc.gridy = gridy;
@@ -240,6 +215,7 @@ public class TenantDashboard extends JFrame {
         gbc.gridy = GridBagConstraints.RELATIVE;
     }
 
+    // removing img with prop details
     private void removeImageFile(String name) {
         File imageFile = new File("Apartments\\" + name + ".jpg");
         if (imageFile.exists()) {
@@ -253,7 +229,10 @@ public class TenantDashboard extends JFrame {
         }
     }
 
-    // public static void main(String[] args) {
-    //     new PropertyDashboard();
-    // }
 }
+/*HAPPY CODING
+If you liked it and it helped you drp a star
+Every Method is open for all just copy and paste
+DM freely on my socials in case you need help
+*/
+
