75a0ee39f45aab5c28caa031dc41ccbc15385789
Mahir Mahmud Siam <mahirmahmudsiam@gmail.com>
2023-12-27 04:30:06 +0600

    Update LandLord.java

--- Landlord/LandLord.java
+++ Landlord/LandLord.java
@@ -43,3 +43,8 @@ public class LandLord {
         return address;
     }
 }
+/*HAPPY CODING
+If you liked it and it helped you drp a star
+Every Method is open for all just copy and paste
+DM freely on my socials in case you need help
+*/
