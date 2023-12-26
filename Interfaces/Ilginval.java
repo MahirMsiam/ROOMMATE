ae8ce4b1ce85d76b2ba37b57bf323313de0b0516
Mahir Mahmud Siam <mahirmahmudsiam@gmail.com>
2023-12-27 04:30:01 +0600

    Update Ilginval.java

--- Interfaces/Ilginval.java
+++ Interfaces/Ilginval.java
@@ -5,3 +5,8 @@ public interface Ilginval {
     boolean validateLoginUser(String user, String pass);
     boolean validateLoginAdmin(String user, String pass);
 }
+/*HAPPY CODING
+If you liked it and it helped you drp a star
+Every Method is open for all just copy and paste
+DM freely on my socials in case you need help
+*/
